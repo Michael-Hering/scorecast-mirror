97e8f5d90336de37fc5b97d5ee7e649a1020b1aa
Ian Matheson <iama2027@colorado.edu>
2020-03-03 11:57:39 -0700

    fixed spacing mistake

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,6 +36,6 @@ resource "google_compute_instance" "default" {
 
 // A variable for extracting the external ip of the instance
 output "ip" {
- value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
 
