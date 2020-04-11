420a56e5b79e307bc54cb7bc80f36d8f48d6554b
Daniel Scott <dasc0325@colorado.edu>
2020-04-11 13:50:34 -0600

    oof, top bar done mostly

--- react-app/src/common/colors/Colors.ts
+++ react-app/src/common/colors/Colors.ts
@@ -1,6 +1,9 @@
 export const Colors = {
-    Obsidian: '#1F1F1F',
+    Obsidian: '#1F1F1F', // for top bar and panel backgrounds
+    LightObsidian: '#2F2F2F', // for profile box
+    BrightBlue: '#00B2FF', //"Scorecast Blue"
     White: '#FFFFFF',
+    Black: '#000000',
 }
 
 export type ColorTypes = typeof Colors
--- react-app/src/dash_components/TopBar.tsx
+++ react-app/src/dash_components/TopBar.tsx
@@ -1,35 +1,18 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Colors } from 'common/colors/Colors'
-import { Logo } from 'common/colors/assets/Logo'
+import { Logo } from 'common/assets/Logo'
+import ProfilePic from 'common/assets/ProfilePic.png'
 
-const TopBarContainer = styled.div`
-    grid-area: topbar;
-
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: row;
-    place-items: center center;
-
-    background-color: ${Colors.Obsidian};
-    box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.4);
-
-    font-family: 'Helvetica Nueue', Roboto, Arial, Helvetica, sans-serif;
-    font-style: normal;
-    color: ${Colors.White};
-    font-size: 20px;
-`
-
-const LogoText = styled.div`
-    font-weight: normal;
-`
-
-const CityText = styled.div`
-    font-weight: bold;
-    margin-left: 5px;
-`
+import {
+    TopBarContainer,
+    ProfileText,
+    LogoText,
+    ProfileBox,
+    CityText,
+    ChangeCity,
+    CityContainer,
+} from 'dash_components/TopBarStyles'
 
 export const TopBar = ({ city }: Props) => {
     return (
@@ -40,10 +23,26 @@ export const TopBar = ({ city }: Props) => {
                     width: 'auto',
                     marginLeft: '5px',
                     marginRight: '10px',
+                    gridArea: 'logo',
                 }}
             />
             <LogoText>Scorecast</LogoText>
-            <CityText>{city}</CityText>
+            <CityContainer>
+                <CityText>{city}</CityText>
+                <ChangeCity>(change city)</ChangeCity>
+            </CityContainer>
+            <ProfileBox>
+                <ProfileText>Profile</ProfileText>
+                <img
+                    src={ProfilePic}
+                    style={{
+                        width: '30px',
+                        height: 'auto',
+                        marginRight: '4px',
+                    }}
+                    alt="profile picture"
+                />
+            </ProfileBox>
         </TopBarContainer>
     )
 }
