459f84ad5267674151a6e65a69e7650993b28cd8
Daniel Scott <dasc0325@colorado.edu>
2020-04-11 16:11:18 -0600

    screen scaling is a little responsive, a little

--- react-app/src/pages/Dashboard.tsx
+++ react-app/src/pages/Dashboard.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import styled from '@emotion/styled'
-import { Colors } from 'common/colors/Colors'
 
 import { DashboardContainer } from 'pages/DashboardStyles'
 import { TopBar } from 'dash_components/TopBar'
--- react-app/src/pages/DashboardStyles.tsx
+++ react-app/src/pages/DashboardStyles.tsx
@@ -5,7 +5,7 @@ export const DashboardContainer = styled.div`
     height: 100%;
 
     display: grid;
-    grid-template-columns: 15% 1fr 1fr 1fr 15%;
+    grid-template-columns: 15vw 1fr 1fr 1fr 15vw;
     grid-template-rows: 50px minmax(0, 1fr) minmax(0, 1fr) 40px;
     column-gap: 1%;
     row-gap: 5%;
@@ -13,7 +13,12 @@ export const DashboardContainer = styled.div`
         'topbar topbar topbar topbar topbar'
         '. weather odds twittertop .'
         '. weather odds twitterbottom .'
-        'footer footer footer footer footer ';
+        '. . . . . ';
+
+    /* Scale gutters for smaller screens */
+    @media screen and (max-width: 1900px) {
+        grid-template-columns: 5vw 1fr 1fr 1fr 5vw;
+    }
 
     background: linear-gradient(119.36deg, #00577c 0%, #003248 100%);
 
