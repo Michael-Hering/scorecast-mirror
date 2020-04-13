6131de55651fafdc66291a5c433c392ba61ca9c5
Michael Hering <michael.hering7@gmail.com>
2020-04-13 12:31:17 -0400

    bump vm type

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ provider "google" {
 // A single Google Cloud Engine instance
 resource "google_compute_instance" "default" {
   name         = "scorecast"
-  machine_type = "f1-micro"
+  machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
   boot_disk {
