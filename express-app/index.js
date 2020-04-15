c4cc1f26fdad41270daa0570137ae928494de741
Ian Matheson <iama2027@colorado.edu>
2020-04-15 15:46:29 -0600

    final changes on dockerizing express-app....localhost brings up react app and localhost:5000/courses will hit the api

--- express-app/index.js
+++ express-app/index.js
@@ -20,15 +20,6 @@ app.get('/ping', (req, res) => {
   return res.send('pong')
 })
 
-//locally serving static build files of react app
-
-// app.use(express.static(path.join(__dirname, '/../react-app/build')))
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/../react-app/build', 'index.html'))
-// })
-
-
 // PORT
 const port = process.env.PORT || 5000
 
