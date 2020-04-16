444a33460b7f49ab826fbbeceac1f24dc32c338c
Ian Matheson <iama2027@colorado.edu>
2020-04-16 11:38:19 -0600

    Initial connection to mongo working and printing out all db's in cluster

--- express-app/index.js
+++ express-app/index.js
@@ -2,9 +2,46 @@
 const express = require('express');
 const app = express();
 const users = require('./routes/users');
-const path = require('path')
+const path = require('path');
+const {MongoClient} = require('mongodb')
+const mongoose = require('mongoose');
 
 //-----------------------------------
+
+//Creating user
+async function createUser(client, newUser){
+  const result = await client.db("users").collection("listingsAndReviews").insertOne(newListing);
+  console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+
+  const uri = "mongodb+srv://dbUser:lHHRGsTDThtYq8zs@scorecast-cluster-iyipd.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
 
 app.use('/api/users', users);
 
