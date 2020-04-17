e3342677b2ecb3ca087fde091f804bb90c630aa0
Michael Hering <michael.hering7@gmail.com>
2020-04-17 19:24:33 -0400

    add route to express app to retreive tweets from kafka queue, enable CORS

--- express-app/index.js
+++ express-app/index.js
@@ -2,11 +2,22 @@
 const express = require('express');
 const app = express();
 const users = require('./routes/users');
+const tweets = require('./routes/tweets');
 const path = require('path')
 
+
 //-----------------------------------
+app.use(express.json())       // to support JSON-encoded bodies
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api/users', users);
+
+app.use('/api/tweets', tweets);
 
 app.get('/ping', (req, res) => {
   return res.send('pong')
