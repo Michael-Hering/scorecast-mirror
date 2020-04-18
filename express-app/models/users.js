11d423864ed6767cfccd8e059a34245b2793737f
Ian Matheson <iama2027@colorado.edu>
2020-04-18 13:14:47 -0600

    updated users schema

--- express-app/models/users.js
+++ express-app/models/users.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  username: String,
-  password: String,
+  email: String,
   bets: [{
-    date: String,
-    completionDate: String,
-    odds: Number,
-    won: Boolean,
+    betId: String
   }],
  });
 
