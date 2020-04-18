33aaa8a2ddc4df09feb20f793b01a03da763b1cf
Ian Matheson <iama2027@colorado.edu>
2020-04-18 10:35:41 -0600

    for loop to sort by timestamp based off of id on document...working correctly now

--- express-app/routes/daily.js
+++ express-app/routes/daily.js
@@ -8,11 +8,22 @@ const Daily = require("../models/daily");
 router.get('/:city', (req, res) => {
   var curCity = req.params.city;
   console.log(curCity)
-  Daily.find({}).select(curCity).sort('-date').limit(1)
+  Daily.find({}).select(curCity)
     .exec()
     .then(doc => {
       newDoc = JSON.parse(JSON.stringify(doc))
-      var nextDayForecast = newDoc[0].denver[1];
+      var recentDate = new Date("02/10/2012"); // setting arbitrary low date
+      for (var i = 0; i < newDoc.length; i++) {
+        //getting timestamp based off of document ID
+        timestamp = newDoc[i]._id.toString().substring(0,8)
+        date = new Date( parseInt( timestamp, 16 ) * 1000 )
+        if(date.getTime() > recentDate){
+          //only grabbing most recentDate and setting doc equal to it
+          recentDate = date;
+          recentDoc = newDoc[i]
+        }
+      }
+      var nextDayForecast = recentDoc;
       res.send(nextDayForecast);
     })
     .catch(err => console.log(err))
--- express-app/routes/hourly.js
+++ express-app/routes/hourly.js
@@ -6,11 +6,22 @@ const Hourly = require("../models/hourly");
 router.get('/:city', (req, res) => {
   var curCity = req.params.city;
   console.log(curCity)
-  Hourly.find({}).select(`${curCity} -_id`).sort('-date').limit(1)
+  Hourly.find({}).select(curCity)
     .exec()
     .then(doc => {
       newDoc = JSON.parse(JSON.stringify(doc))
-      var hourly = newDoc[0].denver;
+      var recentDate = new Date("02/10/2012"); // setting arbitrary low date
+      for (var i = 0; i < newDoc.length; i++) {
+        //getting timestamp based off of document ID
+        timestamp = newDoc[i]._id.toString().substring(0,8)
+        date = new Date( parseInt( timestamp, 16 ) * 1000 )
+        if(date.getTime() > recentDate){
+          //only grabbing most recentDate and setting doc equal to it
+          recentDate = date;
+          recentDoc = newDoc[i]
+        }
+      }
+      var hourly = recentDoc;
       console.log(hourly);
       res.send(hourly);
     })
