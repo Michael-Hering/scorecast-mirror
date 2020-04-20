ad51afa0a4224eba134b43cb5ac1a3dcbf8ee5ec
Daniel Scott <dasc0325@colorado.edu>
2020-04-20 10:44:57 -0600

    removed unused import

--- react-app/src/pages/Dashboard.tsx
+++ react-app/src/pages/Dashboard.tsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import { DashboardContainer } from 'pages/DashboardStyles'
 import { TopBar } from 'dash_components/TopBar'
 
-import { DashPanel } from 'dash_components/DashPanel'
 import { OddsPanel } from 'dash_components/OddsPanel'
 import { WeatherPanel } from 'dash_components/WeatherPanel'
 import { CityPicker } from 'dash_components/CityPicker'
